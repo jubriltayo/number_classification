6ec590f394a8eb4b427a2fbcc1d3eacb2b9df918
Jubril Tayo <78751604+jubriltayo@users.noreply.github.com>
2025-02-05 17:45:40 +0100

    Cleaner logic refactoring

--- main.py
+++ main.py
@@ -19,14 +19,15 @@ async def get_fun_fact(number: int) -> str:
 def is_prime(number: int) -> bool:
     if number < 2:
         return False
-    for i in range(2, abs(number)):
+    for i in range(2, number):
         if number % i == 0:
             return False
     return True
 
 # Check if a number is perfect
 def is_perfect(number: int) -> bool:
-    number = abs(number)
+    if number < 0:
+							return False
     total = sum(i for i in range(1, number) if number % i == 0)
     return total == number
 
